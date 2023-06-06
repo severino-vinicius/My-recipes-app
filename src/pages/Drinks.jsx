8513be2f110edb5b47ed2535df008b21741e6a28
Severino Vinicius <severino.sales95@gmail.com>
2023-06-06 15:45:05 -0300

    [Group10B] - resolução de conflito

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import DrinkIcon from '../images/drinkIcon.svg';
+import Footer from '../components/Footer';
 
 export default function Drinks() {
   return (
@@ -13,6 +14,7 @@ export default function Drinks() {
         pageType="drinks"
       />
       <div>Drinks</div>
+      <Footer />
     </>
   );
 }
--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import MealIcon from '../images/mealIcon.svg';
+import Footer from '../components/Footer';
 
 export default function Meals() {
   return (
@@ -13,6 +14,7 @@ export default function Meals() {
         pageType="meals"
       />
       <div>Meals Page</div>
+      <Footer />
     </>
   );
 }
