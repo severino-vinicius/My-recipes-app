970b9d2d8b2c5598bbda1e28c93d382410b800a1
Matheus Yelgde <yelgde.8@gmail.com>
2023-06-12 14:36:25 -0300

    Requisitos 19 até 23 feitos, faltando os testes

---
+++ src/tests/Recipes.test.js
@@ -0,0 +1,12 @@
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { createMemoryHistory } from 'history';
+// import { Router } from 'react-router-dom';
+// import App from '../App';
+// import Drinks from '../pages/Drinks';
+
+describe('Componente Recipes', () => {
+  it('Os ícones de drinks e refeições no componente levam para suas respectivas páginas', async () => {
+
+  });
+});
