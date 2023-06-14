f6232ab8f4ffb4c9bb416361eae77be3eb60b87b
andrexster <zoio1@hotmail.com>
2023-06-14 15:17:50 -0300

    requisito44-7

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -3,20 +3,22 @@ import Header from '../components/Header';
 
 export default function DoneRecipes() {
   return (
-    <div>
+    <>
       <Header pageTitle="Done Recipes" showSearch={ false } ShowIcon />
-      <button data-testid="filter-by-all-btn">All</button>
-      <button data-testid="filter-by-meal-btn">Meals</button>
-      <button data-testid="filter-by-drink-btn">Drinks</button>
-      <img
-        data-testid={ `${index}-horizontal-image` }
-        src="recipe-image.jpg"
-        alt="Recipe"
-      />
-      <p data-testid={ `${index}-horizontal-top-text` }>Category</p>
-      <p data-testid={ `${index}-horizontal-name` }>Name</p>
-      <p data-testid={ `${index}-horizontal-done-date` }>doneDate</p>
-      <button data-testid={ `${index}-horizontal-share-btn` }>shareBtn</button>
-    </div>
+      <div>
+        <button data-testid="filter-by-all-btn">All</button>
+        <button data-testid="filter-by-meal-btn">Meals</button>
+        <button data-testid="filter-by-drink-btn">Drinks</button>
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src="recipe-image.jpg"
+          alt="Recipe"
+        />
+        <p data-testid={ `${index}-horizontal-top-text` }>Category</p>
+        <p data-testid={ `${index}-horizontal-name` }>Name</p>
+        <p data-testid={ `${index}-horizontal-done-date` }>doneDate</p>
+        <button data-testid={ `${index}-horizontal-share-btn` }>shareBtn</button>
+      </div>
+    </>
   );
 }
