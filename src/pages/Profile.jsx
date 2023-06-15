ec45c1a0e5c859becbc583a7cb9d2ce0a4f8b0c8
louisecerezini <nutrilouiserodrigues@gmail.com>
2023-06-14 22:05:02 -0300

    Comecando o profile- req 57 e 59

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,8 +6,13 @@ export default function Profile() {
   return (
     <>
       <Header pageTitle="Profile" showSearch={ false } showIcon />
-      <div>Profile</div>
       <Footer />
+      <div>
+        <input type="email" data-testid="profile-email"></input>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
     </>
   );
 }
